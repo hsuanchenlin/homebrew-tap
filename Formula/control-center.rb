0ac1c4677ff86270657a660e424fb37f5ff722bb
hsuan-chen lin <hsuanchen@outlook.com>
2026-09-10 08:43:06 +0800

    Update control-center formula to v0.4.0

--- Formula/control-center.rb
+++ Formula/control-center.rb
@@ -1,15 +1,15 @@
 class ControlCenter < Formula
   desc "Local keyboard-first TUI launcher and configurator for CLI tools"
   homepage "https://github.com/hsuanchenlin/control-center"
-  version "0.3.0"
+  version "0.4.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/hsuanchenlin/control-center/releases/download/v0.3.0/control-center_0.3.0_darwin_arm64.tar.gz"
-      sha256 "657d2d1b3f61388bb066557d73b91e8057f655b88b43a607f0c9565a0a36240e"
+      url "https://github.com/hsuanchenlin/control-center/releases/download/v0.4.0/control-center_0.4.0_darwin_arm64.tar.gz"
+      sha256 "7863b749e84b26eb5e7a07c67cd64cf86a2cf3d917788dbaa4779d9ed8e6c66f"
     else
-      url "https://github.com/hsuanchenlin/control-center/releases/download/v0.3.0/control-center_0.3.0_darwin_amd64.tar.gz"
-      sha256 "abf53eded018956477272c5155ddf8a15afed5da76e8f5c4093af2bdab1ae1d8"
+      url "https://github.com/hsuanchenlin/control-center/releases/download/v0.4.0/control-center_0.4.0_darwin_amd64.tar.gz"
+      sha256 "bcd03623c4c29c2859d15820412595235e7fd4d996f1c89a69cfd685badda87b"
     end
   end
 
